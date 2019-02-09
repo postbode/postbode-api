a2e0381a8919e79c6aac26ebff09fdee9e3829da
Mark Hameetman <mark@grivosholding.nl>
2019-02-09 07:46:20 +0100

    Initial commit of Postbode API Client

---
+++ src/Postbode/PostbodeClient.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Postbode;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\RequestOptions;
+
+class PostbodeClient {
+
+    protected $apihost = 'https://app.postbode.nu/api';
+    protected $apikey = null;
+    protected $client = null;
+    protected $version = null;
+    protected $os = null;
+
+    public function __construct($apikey){
+        $this->apikey = $apikey;
+        $this->version = phpversion();
+        $this->os = PHP_OS;
+    }
+
+    protected function getClient(){
+        if(!$this->client){
+            $this->client = new Client([
+                RequestOptions::VERIFY  => true,
+				RequestOptions::TIMEOUT => 30,
+            ]);
+        }
+        return $this->client;
+    }
+
+    protected function sendRequest($method = null, $path = null, $body = null){
+        $client = $this->getClient();
+
+        $options = [
+            RequestOptions::HTTP_ERRORS => false,
+			RequestOptions::HEADERS => [
+				'User-Agent' => "PostbodeClient (PHP Version: ".$this->version.", OS: ".$this->os.")",
+				'Accept' => 'application/json',
+				'Content-Type' => 'application/json',
+				'X-Authorization' => $this->apikey
+			],
+        ];
+
+        if(!empty($body)) {
+
+            $cleanParams = array_filter($body, function($value) {
+				return $value !== null;
+            });
+
+  			switch ($method) {
+				case 'GET':
+				case 'HEAD':
+				case 'DELETE':
+				case 'OPTIONS':
+					$options[RequestOptions::QUERY] = $cleanParams;
+					break;
+				case 'PUT':
+				case 'POST':
+				case 'PATCH':
+					$options[RequestOptions::JSON] = $cleanParams;
+					break;
+			}
+		}
+
+        $response = $client->request($method, $this->apihost . $path, $options);
+
+        $response_code = $response->getStatusCode();
+        if($response_code == 200){
+            return json_decode($response->getBody(), true);
+        }else{
+            return $response_code;
+        }
+    }
+
+    public function getMailboxes(){
+        return $this->sendRequest('GET', '/mailbox');
+    }
+
+    public function getLetters($mailbox_id){
+        return $this->sendRequest('GET', '/mailbox/'.$mailbox_id.'/letters');
+    }
+
+    public function sendLetter($mailbox_id, $filename, $envelope_id = null, $country = null, $registered = false, $send = true){
+        $letter = [
+            'documents' => [
+                [
+                    'name' => basename($filename),
+                    'content' => base64_encode(file_get_contents($filename))
+                ]
+            ],
+            'envelope_id' => $envelope_id,
+            'country' => $country,
+            'registered' => $registered,
+            'send' => $send
+        ];
+
+        return $this->sendRequest('POST', '/mailbox/'.$mailbox_id.'/letters', $letter);
+    }
+
+
+}
+
+?>
